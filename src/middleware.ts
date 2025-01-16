cc25be47c78cf1f88ba37bd9c60fdac648a171db
Abhishek Soni <abhisoni1764@gmail.com>
2025-01-17 04:25:55 +0530

    Update middleware to include authentication secret and improve path handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
-  const url = request.nextUrl;
+  const token = await getToken({
+    req: request,
+    secret: process.env.AUTH_SECRET_TOKEN,
+  });
+  const { pathname } = request.nextUrl;
+
   if (
     token &&
-    (url.pathname.startsWith("/sign-in") ||
-      url.pathname.startsWith("/sign-up") ||
-      url.pathname.startsWith("/verify") ||
-      url.pathname.startsWith("/"))
+    ["/sign-in", "/sign-up", "/verify"].some((path) =>
+      pathname.startsWith(path)
+    )
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (!token && url.pathname.startsWith("/dashboard")) {
+  if (!token && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
 
@@ -24,5 +26,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/", "/verify/:path*", "/dashboard/:path*"],
+  matcher: ["/sign-in", "/sign-up", "/verify/:path*", "/dashboard/:path*"],
 };
