914ab5e501a66f35310f7cf222eb63b8efb98357
Abhishek Soni <abhisoni1764@gmail.com>
2025-01-16 19:22:43 +0530

    Add user verification page with form handling and toast notifications

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,113 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
+import { verifySchema } from "@/schemas/verifySchema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { Loader2 } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+const VerifyUser = () => {
+  const [isVerifying, setIsVerifying] = useState(false);
+  const router = useRouter();
+  const params = useParams<{ username: string }>();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof verifySchema>>({
+    resolver: zodResolver(verifySchema),
+  });
+
+  const onSubmit = async (data: z.infer<typeof verifySchema>) => {
+    setIsVerifying(true);
+    try {
+      const response = await axios.post<ApiResponse>(`/api/verify-code`, {
+        username: params.username,
+        code: data.code,
+      });
+
+      if (response.data.success === true) {
+        toast({
+          title: "User is Verified!!",
+          description: response?.data.message,
+        });
+
+        router.replace("/sign-in");
+      } else {
+        toast({
+          title: "User not Verified!!",
+          description: response?.data.message,
+        });
+      }
+    } catch (error) {
+      console.error("Error while Verifying User!!");
+      const axiosError = error as AxiosError<ApiResponse>;
+      const errorMessage = axiosError.response?.data.message;
+
+      toast({
+        title: "Failed To Verify User!!",
+        description: errorMessage,
+        variant: "destructive",
+      });
+    } finally {
+      setIsVerifying(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Please Verify Your Account!
+          </h1>
+          <p className="mb-4">
+            Enter the Verification Code sent to your email.
+          </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="code"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Verification Code</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter Verification Code.." {...field} />
+                  </FormControl>
+                  {isVerifying && <Loader2 className="animate-spin" />}
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isVerifying}>
+              {isVerifying ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                  Please wait
+                </>
+              ) : (
+                "Sign Up"
+              )}
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default VerifyUser;
